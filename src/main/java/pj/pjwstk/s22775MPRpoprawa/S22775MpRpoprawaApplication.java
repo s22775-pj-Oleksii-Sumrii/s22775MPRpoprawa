0630a04c2c2ea479228c4edb2fb791bca3ba613b
s22775-pj-Oleksii-Sumrii <74150190+s22775-pj-Oleksii-Sumrii@users.noreply.github.com>
2024-02-01 18:10:52 +0100

    https://github.com/s22775-pj-Oleksii-Sumrii/s22775MRPpoprawa.git

---
+++ src/main/java/pj/pjwstk/s22775MPRpoprawa/S22775MpRpoprawaApplication.java
@@ -0,0 +1,13 @@
+package pj.pjwstk.s22775MPRpoprawa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S22775MpRpoprawaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S22775MpRpoprawaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pj/pjwstk/s22775MPRpoprawa/S22775MpRpoprawaApplicationTests.java
@@ -0,0 +1,13 @@
+package pj.pjwstk.s22775MPRpoprawa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S22775MpRpoprawaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
